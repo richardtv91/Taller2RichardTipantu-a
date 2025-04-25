c30791c7bf502bf669ad757898d5e20f0a47de17
master <master@SMLAB306PC14_25>
2025-04-24 21:18:45 -0500

    Agregar archivos de proyecto.

---
+++ Views/Vista1.xaml.cs
@@ -0,0 +1,9 @@
+namespace Taller2RichardTipantuña.Views;
+
+public partial class Vista1 : ContentView
+{
+	public Vista1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/Vista2.xaml.cs
@@ -0,0 +1,9 @@
+namespace Taller2RichardTipantuña.Views;
+
+public partial class Vista2 : ContentView
+{
+	public Vista2()
+	{
+		InitializeComponent();
+	}
+}
